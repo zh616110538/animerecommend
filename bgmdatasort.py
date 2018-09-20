d99ed78a43eb9f789cefbc935fbf4c40564d6c20
zhu <>
2018-09-20 19:47:08 +0800

    爬取了所有信息

--- bgmdatasort.py
+++ bgmdatasort.py
@@ -5,7 +5,6 @@ import numpy as np
 import requests
 import json
 import time
-import tensorflow as tf
 
 def readData(filesource,start,end):
     data = []
@@ -44,32 +43,34 @@ def genUserTable(data,subject):
     #     vec[user[0]] = u
     mat = np.zeros((m,n))
     count = 0
-    us = []
     for user in data:
         for i in user[1:]:
             mat[count][subject[i[0]]] = int(i[1])
-        us.append(user[0])
         count+=1
-    return mat,np.array(us)
+    return mat
 
 
 def caculate(usertb,subject,user):
     index = []
     curuser = []
-    caltime = [time.time()]
     #生成用户的向量
     for item in user.items():
         index.append(subject[item[0]])
         curuser.append(item[1])
-    curuser = np.array(curuser,dtype=np.int8)
-    caltime.append(time.time())
+    curuser = np.array(curuser,dtype=np.float64)
     #生成所有用户对应当前用户的矩阵
     temptb = usertb[:,[subject[i[0]] for i in user.items()]]
-    caltime.append(time.time())
-    dis = 1/(1+np.linalg.norm(temptb-curuser,axis = -1))
-    caltime.append(time.time())
-    for i in range(1,len(caltime)):
-        print("time is %s"% (caltime[i] - caltime[i-1]))
+    # dis = 1/(1+np.linalg.norm(temptb-curuser,axis = -1))
+    dis = np.zeros((len(temptb),))
+    for i in range(0,len(temptb)):
+        if sum(temptb[i]) != 0:
+            try:
+                dis[i] = 0.5 + 0.5 * np.corrcoef(temptb[i],curuser, rowvar = False)[0][1]
+            except Exception:
+                print(i)
+            # print(i)
+        else:
+            dis[i] = 0
     return dis
 
 def genmaxlist(dis,n):
@@ -89,32 +90,28 @@ def getName(id):
 
 data = readData('userdata/BangumiUser',1,10)
 subject,resubject = genDic(data)
+usertb = genUserTable(data,subject)
 
-usertb,us = genUserTable(data,subject)
+def genlist(zhuhao,num):
+    sim = 5
+    dis = caculate(usertb,subject,zhuhao)
+    simuser = genmaxlist(dis,30)
+    # simuser = np.nan_to_num(simuser)
+    # print(simuser)
+    a = np.zeros((len(subject),),dtype=np.float64)
+    for user in simuser[:sim]:
+        a += usertb[user[0]]*user[1]
+    a = a/sim
+    xx = []
+    for i in range(0,len(a)):
+        if not resubject[i] in zhuhao:
+            xx.append((resubject[i],a[i]))
+    ff = sorted(xx,key = lambda s:s[1],reverse=True)
+    t2 = time.time()
+    for i in ff[:num]:
+        print('%s  simlarity:%f'%(getName(i[0]),i[1]))
 
-t1 = time.time()
-zhuhao = {}
-zhuhao['112146'] = 8
-zhuhao['100449'] = 9
-zhuhao['211936'] = 9
-zhuhao['110467'] = 10
-zhuhao['102134'] = 9
-
-dis = caculate(usertb,subject,zhuhao)
-simuser = genmaxlist(dis,30)
-
-a = np.zeros((len(subject),),dtype=np.float64)
-for user in simuser[:10]:
-    a += usertb[user[0]]*user[1]
-a = a/10
-
-xx = []
-for i in range(0,len(a)):
-    if not resubject[i] in zhuhao:
-        xx.append((resubject[i],a[i]))
-
-ff = sorted(xx,key = lambda s:s[1],reverse=True)
-t2 = time.time()
-print(t2-t1)
-for i in ff[:20]:
-    print('%s  subject:%s simlarity:%f'%(getName(i[0]),i[0],i[1]))
+guzi = {'100449':9,'211936':7,'91986':7,'793':6,'231722':6,'112146':6}
+yaoyao = {'9717':9.7,'10639':9.7,'10380':9.6,'88287':9.4,'1428':9.5,'140001':9.3,'10440':9,'172498':6,'204855':6}
+zhuhao = {'112146':8,'5436':9,'211936':8,'214265':8,'110467':10,'805':9}
+genlist(zhuhao,20)
